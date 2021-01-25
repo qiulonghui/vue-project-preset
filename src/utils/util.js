1a418ece17962de56f07df94d95b604b9a2999ed
Qiulonghui <569374939@qq.com>
2021-01-25 14:25:44 +0800

    增加bodyscrolllock

--- src/utils/util.js
+++ src/utils/util.js
@@ -3,3 +3,11 @@ import dayjs from 'dayjs'
 export function formatTime(time, formatStr = 'YYYY-MM-DD') {
   return dayjs(time).format(formatStr)
 }
+
+export function getUrlParams (param) {
+	const reg = new RegExp('(^|&)' + param + '=([^&]*)(&|$)','i');
+	const r = window.location.search.substr(1).match(reg) || window.location.hash.substring((window.location.hash.search(/\?/)) + 1).match(reg);
+	if (r != null) {
+			return decodeURIComponent(r[2]);
+	}
+};
